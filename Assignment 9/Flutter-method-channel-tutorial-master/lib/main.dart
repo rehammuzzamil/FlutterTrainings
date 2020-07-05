cd41460f05af080b65021a7c239038d37a8ba7d1
rehammuzzamil <reham.muzzamil@venturedive.com>
2020-07-05 14:02:26 +0500

    Completed TODOS Assignment 9

---
+++ Assignment 9/Flutter-method-channel-tutorial-master/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new Scaffold(
+      body: new PlatformTestBody(),
+    ),
+  ));
+}
+
+class PlatformTestBody extends StatefulWidget {
+  @override
+  PlatformTestBodyState createState() {
+    return new PlatformTestBodyState();
+  }
+}
+
+class PlatformTestBodyState extends State<PlatformTestBody> {
+  static const platformMethodChannel = const MethodChannel('com.test/test');
+  String nativeMessage = '';
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.pinkAccent,
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          new Padding(
+            padding: const EdgeInsets.only(left: 18.0, top: 200.0),
+            child: new Text(
+              'Tap the button to change your life!',
+              style: new TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 23.0),
+            ),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 102.0),
+            child: new RaisedButton(
+              child: new Text('Click Me'),
+              onPressed: () => doNativeSuff(),
+            ),
+          ),
+          new Padding(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 102.0),
+            child: new Text(
+              nativeMessage,
+              style: new TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 23.0),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  /**
+   * TODO Handle the code below from line 81-86 in try-catch.
+   * TODO assign an appropriate value to result in catch.
+   */
+
+  /**
+   * TODO use platformMethodChannel to invoke vibrateDevice methode.
+   * TODO assign the return from platformMethodChannel to variable "$result"
+   */
+
+  Future<Null> doNativeSuff() async {
+    String _message;
+    try {
+      final String result =
+          await platformMethodChannel.invokeMethod('vibrateDevice');
+      _message = result;
+      print(result);
+    } on PlatformException catch (e) {
+      _message = "Sadly I can not vibrate: ${e.message}.";
+    }
+    setState(() {
+      nativeMessage = _message;
+    });
+  }
+}
